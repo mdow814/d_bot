6541ca524a8ccb038aaa66d131936d377ad9ee50
Michael Downey <mdow814@gmail.com>
2021-07-04 17:12:12 -0400

    adding d_bot

---
+++ src/main.rs
@@ -0,0 +1,58 @@
+mod utils;
+
+use std::env;
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+
+#[group]
+#[commands(ping, build)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~"))
+        .group(&GENERAL_GROUP);
+
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn build(ctx: &Context, msg: &Message) -> CommandResult {
+    let build = utils::builds::get_build();
+    let message = build.get_items();
+    msg.reply(ctx, message).await?;
+
+    Ok(())
+}
---
+++ src/utils/builds.rs
@@ -0,0 +1,46 @@
+use std::fs;
+use serde::{Serialize, Deserialize};
+use rand::Rng;
+use std::convert::TryFrom;
+
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+pub struct Build {
+    item1: String,
+    item2: String,
+    item3: String,
+    item4: String,
+    item5: String,
+    item6: String,
+}
+
+impl Build {
+
+    pub fn get_items(self) -> String {
+        let message = format!(
+            "Here's your build!\n\n{}\n{}\n{}\n{}\n{}\n{}\n",
+            self.item1,
+            self.item2,
+            self.item3,
+            self.item4,
+            self.item5,
+            self.item6,
+        );
+        message
+    }
+}
+
+fn get_num() -> u32 {
+    let mut rng = rand::thread_rng();
+    rng.gen_range(0..150)
+}
+
+pub fn get_build() -> Box<Build> {
+    let num = get_num();
+    let build_file_string = fs::read_to_string("C:\\Users\\mdow8\\CLionProjects\\d_bot\\src\\utils\\builds.yaml").unwrap();
+    let mut b: Vec<Build> = serde_yaml::from_str(&build_file_string).unwrap();
+    let index = usize::try_from(num).unwrap();
+    let build = b.remove(index);
+    Box::from(build)
+}
+
